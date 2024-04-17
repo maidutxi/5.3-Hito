ed9a61d23848cd2715f6d1f09ede0619a6614e14
Maider Belver <mbelvermi23dw@ikzubirimanteo.com>
2024-04-17 16:26:05 +0200

    crear 3.1,3.2 y 3.3

--- src/test/java/com.pruebas/SmarthPhoneServiceImplTes.java
+++ src/test/java/com.pruebas/SmarthPhoneServiceImplTes.java
@@ -1,2 +1,28 @@
 public class SmarthPhoneServiceImplTes {
+    private SmartPhoneServiceImpl smartPhoneService;
+
+
+    @BeforeEach
+    public void setUp() {
+        smartPhoneService = new SmartPhoneServiceImpl();
+    }
+    @Test
+    public void testCountNotNull() {
+        assertNotNull(smartPhoneService.count());
+    }
+
+
+    @Test
+    public void testCountGreaterThanZero() {
+        assertTrue(smartPhoneService.count() > 0);
+    }
+
+
+    @Test
+    public void testCountEqualsThree() {
+        assertEquals(3, smartPhoneService.count());
+    }
+
+
+
 }
