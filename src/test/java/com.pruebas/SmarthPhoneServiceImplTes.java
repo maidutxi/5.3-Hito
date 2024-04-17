797dad50281c8ab7858e17f578c654494d79f081
Maider Belver <mbelvermi23dw@ikzubirimanteo.com>
2024-04-17 16:32:03 +0200

    poner nombre con displayName

--- src/test/java/com.pruebas/SmarthPhoneServiceImplTes.java
+++ src/test/java/com.pruebas/SmarthPhoneServiceImplTes.java
@@ -1,4 +1,20 @@
-public class SmarthPhoneServiceImplTes {
+package com.pruebas;
+import static org.junit.jupiter.api.Assertions.*;
+
+
+import com.example.demo.service.SmartPhoneServiceImpl;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.*;
+import static org.junit.jupiter.api.Assertions.*;
+
+
+@TestMethodOrder(OrderAnnotation.class)
+@DisplayName("Conjunto de pruebas")
+public class SmartPhoneServiceImplTest {
+
+
     private SmartPhoneServiceImpl smartPhoneService;
 
 
@@ -6,30 +22,42 @@ public class SmarthPhoneServiceImplTes {
     public void setUp() {
         smartPhoneService = new SmartPhoneServiceImpl();
     }
+
+
+
+
     @Test
+    @DisplayName("No es Nulo")
     public void testCountNotNull() {
         assertNotNull(smartPhoneService.count());
     }
 
 
     @Test
+    @DisplayName("Mayor que 0")
     public void testCountGreaterThanZero() {
         assertTrue(smartPhoneService.count() > 0);
     }
 
 
     @Test
+    @DisplayName("Igual a 3")
     public void testCountEqualsThree() {
         assertEquals(3, smartPhoneService.count());
     }
 
+
     @Test
+    @DisplayName("ID nulo")
     public void testFindOneWithNullId() {
         assertThrows(IllegalArgumentException.class, () -> {
             smartPhoneService.findOne(null);
         });
     }
+
+
     @Test
+    @DisplayName("Contador")
     public void testCount() {
         assertAll("count",
                 () -> assertNotNull(smartPhoneService.count()),
@@ -37,6 +65,4 @@ public class SmarthPhoneServiceImplTes {
                 () -> assertEquals(3, smartPhoneService.count())
         );
     }
-
-
 }
